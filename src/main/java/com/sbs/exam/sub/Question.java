ad77d44c83002c06138947d11c6b04d454e9ad26
Mac <mac@Macui-MacBookAir.local>
2024-07-31 15:06:12 +0900

    CascadeType.REMOVE

--- src/main/java/com/sbs/exam/sub/Question.java
+++ src/main/java/com/sbs/exam/sub/Question.java
@@ -3,6 +3,7 @@ package com.sbs.exam.sub;
 import jakarta.persistence.*;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
 //엔티티 클래스 => 아래 클래스와 1:1로 매칭되는 테이블이 DB에 없다면,자동 생성.
@@ -18,5 +19,10 @@ public class Question {
     private String content;
 
     private LocalDateTime createDAte;  //Datetime
+
+    @OneToMany(mappedBy = "question",cascade = CascadeType.REMOVE)
+          //Anwser클라스에 있는 question변수    // 직문을 삭제시 모든 답변도 같이 삭제됨
+    private List<Answer> answerList;
+
 
 }
